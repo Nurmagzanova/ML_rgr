ee971dcf08323afe16288bcc73fbdd4cd592200c
Nurmagzanova <kamilanzh@mail.ru>
2024-01-11 19:35:15 +0600

    1

--- pages/2_ВИЗУАЛИЗАЦИЯ.py
+++ pages/2_ВИЗУАЛИЗАЦИЯ.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data= pd.read_csv("DataSet4_fil.xls")
+data= pd.read_csv("DataSet4_fil.csv")
 
 st.title('Визуализация датасета')
 
